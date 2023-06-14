4c0bb4fd5ba4313b5b07d6d4e48ee2388c84082c
Fábio Kenji Matsuda <fabio@fkmatsuda.dev>
2023-06-14 14:27:21 -0300

    Feat/slice implementations (#7)
    * fix: disable inspection on sonar-project.properties
    
    * feat: implements some useful functions for slices

--- slices/SLICES.md
+++ slices/SLICES.md
@@ -13,7 +13,114 @@ To use the "slices" package, import it in your Go code:
 import "github.com/fkmatsuda-dev/commons/slices"
 ```
 
-To find an element index in a slice, use the slices.FindInSlice() function:
+## Functions
+
+### InterfaceSlice
+InterfaceSlice cast any slices to []interface{}
+#### Original source https://stackoverflow.com/questions/12753805/type-converting-slices-of-interfaces
+```go
+slice := []string{"a", "b", "c"}
+interfaceSlice := slices.InterfaceSlice(slice)
+fmt.Println(interfaceSlice) // [a b c]
+```
+
+### Filter
+Filter takes a slices and filters it based on the function passed in.
+```go
+slice := []string{"a", "b", "c"}
+filteredSlice := slices.Filter(slice, func(item string) bool {
+    return item != "b"
+})
+fmt.Println(filteredSlice) // [a c]
+```
+
+### Find
+Find takes a slices and looks for an element in it. If found it will return it's index, otherwise it will return -1 and a bool of false.
+```go
+slice := []string{"a", "b", "c"}
+index, found := slices.Find(slice, "b)
+if found {
+    fmt.Println(index) // 1
+}
+```
+
+### FindFunc
+FindFunc takes a slices and looks for an element in it using a function to compare. If found it will return it's index, otherwise it will return -1 and a bool of false.
+```go
+slice := []string{"a", "b", "c"}
+value := "b"
+
+index, found := slices.FindFunc(slice, func(item string) bool {
+    return item == value
+})
+if found {
+    fmt.Println(index) // 1
+}
+```
+
+### IndexOf
+IndexOf returns the index of the first instance of the given value, or -1 if not found.
+```go
+slice := []string{"a", "b", "c"}
+index := slices.IndexOf(slice, "b")
+fmt.Println(index) // 1
+```
+
+### IndexOfFunc
+IndexOfFunc returns the index of the first instance using a function to compare, or -1 if not found.
+```go
+slice := []string{"a", "b", "c"}
+value := "b"
+
+index := slices.IndexOfFunc(slice, func(item string) bool {
+    return item == value
+})
+fmt.Println(index) // 1
+```
+
+### Contains
+Contains returns true if the given value exists in the slice.
+```go
+slice := []string{"a", "b", "c"}
+contains := slices.Contains(slice, "b")
+fmt.Println(contains) // true
+```
+
+### ContainsFunc
+ContainsFunc returns true if the comparison function finds an instance among the elements.
+```go
+slice := []string{"a", "b", "c"}
+value := "b"
+
+contains := slices.ContainsFunc(slice, func(item string) bool {
+    return item == value
+})
+fmt.Println(contains) // true
+```
+
+### LastIndexOf
+LastIndexOf returns the index of the last instance of the given value, or -1 if not found.
+```go
+slice := []string{"a", "b", "c", "b"}
+index := slices.LastIndexOf(slice, "b")
+fmt.Println(index) // 3
+```
+
+### LastIndexOfFunc
+LastIndexOfFunc returns the index of the last instance using a function to compare, or -1 if not found.
+```go
+slice := []string{"a", "b", "c", "b"}
+value := "b"
+
+index := slices.LastIndexOfFunc(slice, func(item string) bool {
+    return item == value
+})
+fmt.Println(index) // 3
+```
+
+### FindInSlice
+FindInSlice takes a slices and looks for an element in it. If found it will return it's index, otherwise it will return -1 and a bool of false.
+#### Consider using the find function described above
 ```go
 slice := []string{"a", "b", "c"}
 index, found := slices.FindInSlice(slice, "b")
@@ -22,7 +129,9 @@ if found {
 }
 ```
 
-You can also find an element index in a slice using a custom comparator function:
+### FindInSliceFunc
+FindInSliceFunc takes a slices and looks for an element in it using a function to compare. If found it will return it's index, otherwise it will
+#### Consider using the find function described above
 ```go
 slice := []string{"a", "b", "c"}
 value := "b"
@@ -33,11 +142,4 @@ index, found := slices.FindInSliceFunc(slice, func(item string) bool {
 if found {
     fmt.Println(index) // 1
 }
-```
-
-If you need to convert a slice of some type to a slice of interface{}, use the slices.InterfaceSlice() function:
-```go
-slice := []string{"a", "b", "c"}
-interfaceSlice := slices.InterfaceSlice(slice)
-fmt.Println(interfaceSlice) // [a b c]
 ```
--- slices/slice_test.go
+++ slices/slice_test.go
@@ -28,63 +28,6 @@ func TestFindInSliceInt(t *testing.T) {
 	}
 }
 
-func TestFindInSliceString(t *testing.T) {
-	slice := []string{"alpha", "beta", "gamma", "theta"}
-	var (
-		idx   int
-		found bool
-	)
-	idx, found = FindInSlice(slice, "alpha")
-	if !found {
-		t.Error("\"alpha\" exists in slices")
-	}
-	if idx != 0 {
-		t.Error("\"alpha\" must be in index 0")
-	}
-	idx, found = FindInSlice(slice, "gamma")
-	if !found {
-		t.Error("\"gamma\" exists in slices")
-	}
-	if idx != 2 {
-		t.Error("\"gamma\" must be in index 2")
-	}
-	_, found = FindInSlice(slice, "delta")
-	if found {
-		t.Error("\"delta\" not exists in slices")
-	}
-}
-
-type testStruct struct {
-	str   string
-	value int
-}
-
-func TestFindInSliceStruct(t *testing.T) {
-	slice := []testStruct{{"alpha", 1}, {"beta", 5}, {"gamma", 7}, {"theta", 8}}
-	var (
-		idx   int
-		found bool
-	)
-	idx, found = FindInSlice(slice, testStruct{"alpha", 1})
-	if !found {
-		t.Error("\"alpha\" exists in slices")
-	}
-	if idx != 0 {
-		t.Error("\"alpha\" must be in index 0")
-	}
-	idx, found = FindInSlice(slice, testStruct{"gamma", 7})
-	if !found {
-		t.Error("\"gamma\" exists in slices")
-	}
-	if idx != 2 {
-		t.Error("\"gamma\" must be in index 2")
-	}
-	_, found = FindInSlice(slice, testStruct{"delta", 9})
-	if found {
-		t.Error("\"delta\" not exists in slices")
-	}
-}
-
 func TestInterfaceSliceWithThreeElements(t *testing.T) {
 	slice := []int{1, 2, 3}
 	iSlice := InterfaceSlice(slice)
@@ -119,5 +62,205 @@ func TestInterfaceSliceNil(t *testing.T) {
 	var slice []int
 	if InterfaceSlice(slice) != nil {
 		t.Error("InterfaceSlice() should return nil")
+	}
+}
+
+func TestFind(t *testing.T) {
+	slice := []int{1, 5, 7, 8}
+	var (
+		idx   int
+		found bool
+	)
+	idx, found = Find(slice, 1)
+	if !found {
+		t.Error("\"1\" exists in slices")
+	}
+	if idx != 0 {
+		t.Error("\"1\" must be in index 0")
+	}
+	idx, found = Find(slice, 7)
+	if !found {
+		t.Error("\"7\" exists in slices")
+	}
+	if idx != 2 {
+		t.Error("\"7\" must be in index 2")
+	}
+	_, found = Find(slice, 9)
+	if found {
+		t.Error("\"9\" not exists in slices")
+	}
+}
+
+func TestFindFunc(t *testing.T) {
+	slice := []int{1, 5, 7, 8}
+	var (
+		idx   int
+		found bool
+	)
+	idx, found = FindFunc(slice, func(i int) bool { return i == 1 })
+	if !found {
+		t.Error("\"1\" exists in slices")
+	}
+	if idx != 0 {
+		t.Error("\"1\" must be in index 0")
+	}
+	idx, found = FindFunc(slice, func(i int) bool { return i == 7 })
+	if !found {
+		t.Error("\"7\" exists in slices")
+	}
+	if idx != 2 {
+		t.Error("\"7\" must be in index 2")
+	}
+	_, found = FindFunc(slice, func(i int) bool { return i == 9 })
+	if found {
+		t.Error("\"9\" not exists in slices")
+	}
+}
+
+func TestIndexOf(t *testing.T) {
+	slice := []int{1, 5, 7, 8}
+	var idx int
+	idx = IndexOf(slice, 1)
+	if idx != 0 {
+		t.Error("\"1\" must be in index 0")
+	}
+	idx = IndexOf(slice, 7)
+	if idx != 2 {
+		t.Error("\"7\" must be in index 2")
+	}
+	idx = IndexOf(slice, 9)
+	if idx != -1 {
+		t.Error("\"9\" not exists in slices")
+	}
+}
+
+func TestIndexOfFunc(t *testing.T) {
+	slice := []int{1, 5, 7, 8}
+	var idx int
+	idx = IndexOfFunc(slice, func(i int) bool { return i == 1 })
+	if idx != 0 {
+		t.Error("\"1\" must be in index 0")
+	}
+	idx = IndexOfFunc(slice, func(i int) bool { return i == 7 })
+	if idx != 2 {
+		t.Error("\"7\" must be in index 2")
+	}
+	idx = IndexOfFunc(slice, func(i int) bool { return i == 9 })
+	if idx != -1 {
+		t.Error("\"9\" not exists in slices")
+	}
+}
+
+func TestLastIndexOf(t *testing.T) {
+	slice := []int{1, 5, 7, 8, 5}
+	var idx int
+	idx = LastIndexOf(slice, 1)
+	if idx != 0 {
+		t.Error("\"1\" must be in index 0")
+	}
+	idx = LastIndexOf(slice, 5)
+	if idx != 4 {
+		t.Error("\"5\" must be in index 4")
+	}
+	idx = LastIndexOf(slice, 9)
+	if idx != -1 {
+		t.Error("\"9\" not exists in slices")
+	}
+}
+
+func TestFilter(t *testing.T) {
+	slice := []int{1, 5, 7, 8}
+	filtered := Filter(slice, func(i int) bool { return i > 5 })
+	if len(filtered) != 2 {
+		t.Error("Filtered slices must have 2 elements")
+	}
+	if filtered[0] != 7 {
+		t.Error("Filtered slices must have 7 as 1st element")
+	}
+	if filtered[1] != 8 {
+		t.Error("Filtered slices must have 8 as 2nd element")
+	}
+}
+
+func TestContains(t *testing.T) {
+	slice := []int{1, 5, 7, 8}
+	if !Contains(slice, 1) {
+		t.Error("\"1\" exists in slices")
+	}
+	if !Contains(slice, 7) {
+		t.Error("\"7\" exists in slices")
+	}
+	if Contains(slice, 9) {
+		t.Error("\"9\" not exists in slices")
+	}
+}
+
+func TestContainsFunc(t *testing.T) {
+	slice := []int{1, 5, 7, 8}
+	if !ContainsFunc(slice, func(i int) bool { return i == 1 }) {
+		t.Error("\"1\" exists in slices")
+	}
+	if !ContainsFunc(slice, func(i int) bool { return i == 7 }) {
+		t.Error("\"7\" exists in slices")
+	}
+	if ContainsFunc(slice, func(i int) bool { return i == 9 }) {
+		t.Error("\"9\" not exists in slices")
+	}
+}
+
+func TestFindString(t *testing.T) {
+	slice := []string{"alpha", "beta", "gamma", "theta"}
+	var (
+		idx   int
+		found bool
+	)
+	idx, found = Find(slice, "alpha")
+	if !found {
+		t.Error("\"alpha\" exists in slices")
+	}
+	if idx != 0 {
+		t.Error("\"alpha\" must be in index 0")
+	}
+	idx, found = Find(slice, "gamma")
+	if !found {
+		t.Error("\"gamma\" exists in slices")
+	}
+	if idx != 2 {
+		t.Error("\"gamma\" must be in index 2")
+	}
+	_, found = Find(slice, "delta")
+	if found {
+		t.Error("\"delta\" not exists in slices")
+	}
+}
+
+type testStruct struct {
+	str   string
+	value int
+}
+
+func TestFindStruct(t *testing.T) {
+	slice := []testStruct{{"alpha", 1}, {"beta", 5}, {"gamma", 7}, {"theta", 8}}
+	var (
+		idx   int
+		found bool
+	)
+	idx, found = Find(slice, testStruct{"alpha", 1})
+	if !found {
+		t.Error("\"alpha\" exists in slices")
+	}
+	if idx != 0 {
+		t.Error("\"alpha\" must be in index 0")
+	}
+	idx, found = Find(slice, testStruct{"gamma", 7})
+	if !found {
+		t.Error("\"gamma\" exists in slices")
+	}
+	if idx != 2 {
+		t.Error("\"gamma\" must be in index 2")
+	}
+	_, found = Find(slice, testStruct{"delta", 9})
+	if found {
+		t.Error("\"delta\" not exists in slices")
 	}
 }
